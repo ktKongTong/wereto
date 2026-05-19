aeb6247200e7a4a04c31742b989f6a3c90fd8f85
ktkongtong <44502608+ktKongTong@users.noreply.github.com>
2026-05-19 22:03:50 +0800

    init commit

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,13 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite-plus";
-import { reactRouterHonoServer } from "react-router-hono-server/dev";
 
 export default defineConfig(() => {
   return {
     plugins: [
       cloudflare({
         viteEnvironment: { name: "ssr" },
+        configPath: '../../wrangler.jsonc'
       })
       , tailwindcss(), reactRouter()],
     resolve: {
